ae1c07758ef89bd026c3334643feb16d1cc0a090
DFenniak <donald@getjobber.com>
2017-04-12 19:45:07 -0600

    missed the other part of challenge 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ItemsComponent } from './items/items.component';
+import { GizmoComponent } from './gizmo/gizmo.component';
 import { WidgetsComponent } from './widgets/widgets.component';
 import { FeaturedItemComponent } from './items/featured-item/featured-item.component';
 import { FeaturedWidgetComponent } from './widgets/featured-widget/featured-widget.component';
 
 const routes: Routes = [
   {path: '',      redirectTo: '/items', pathMatch: 'full' },
+  {path: 'gizmo', component: GizmoComponent },
   {path: 'items', component: ItemsComponent, children: [
     {path: 'featured/:id', component: FeaturedItemComponent}
   ]},
