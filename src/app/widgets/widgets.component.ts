57b763c2b352389cc48e9a0d854e9b66da13b232
DFenniak <donald@getjobber.com>
2017-04-18 22:47:08 -0600

    widgey selection _bish_

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -7,5 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class WidgetsComponent implements OnInit {
-  ngOnInit() { }
+  ngOnInit() {
+    this.widgets = [
+      {id: 1, name: "widget 1"},
+      {id: 2, name: "widget 2"},
+      {id: 3, name: "widget 3"},
+      {id: 4, name: "widget 4"},
+      {id: 5, name: "widget 5"}
+    ];
+  }
+  uclikmuhwidgey(widge: Object) {
+    this.selectedWidget = widge
+  };
+  widgets: Array<Object>;
+  selectedWidget: Object;
 }
